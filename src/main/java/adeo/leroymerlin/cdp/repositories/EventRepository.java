f2d797683079585414c330c471118361350f3020
bounhh <bo_zakaria@hotmail.com>
2022-12-19 20:13:20 +0100

    Fix Delete API

--- src/main/java/adeo/leroymerlin/cdp/repositories/EventRepository.java
+++ src/main/java/adeo/leroymerlin/cdp/repositories/EventRepository.java
@@ -1,15 +1,20 @@
 package adeo.leroymerlin.cdp.repositories;
 
 import adeo.leroymerlin.cdp.entities.Event;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional(readOnly = true)
+@Transactional
 public interface EventRepository extends Repository<Event, Long> {
 
-    void delete(Long eventId);
+    @Modifying
+    @Query("delete from Event e where e.id=:eventId")
+    void delete(@Param("eventId") Long eventId);
 
     List<Event> findAllBy();
 }
